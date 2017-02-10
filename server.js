fd808d5f5cbc369e27290b82ebd6f352c1924207
jankosacc <janis.koselevs@accenture.com>
2017-02-10 09:18:36 +0200

    Adding files

---
+++ server.js
@@ -0,0 +1,46 @@
+require('dotenv').config();
+
+var express = require('express');
+var cors = require('cors');
+var path = require('path');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'images')));
+
+app.use(require('./routes/api'));
+
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+app.listen(process.env.PORT || '8081', function() {
+  console.log("Express server listening on port %d in %s mode", process.env.PORT || '8081', app.get('env'));
+});
